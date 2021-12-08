01b0b63823468a0c82c3c34669e59cd9d54e3a3a
Daniel Sitarek <deltasierra777@gmail.com>
2021-12-07 22:16:50 -0600

    finished player details card except style

--- src/components/DefenseDetailsCard.js
+++ src/components/DefenseDetailsCard.js
@@ -3,28 +3,27 @@ import PropTypes from 'prop-types';
 
 export default function DefenseDetailsCard({ playerDetails, roundNum }) {
   return (
-    <div>
-      <li>Games: {playerDetails.games}</li>
-      <li>Games Started: {playerDetails.games_started}</li>
-      <li>Time on Ice: {roundNum(playerDetails.time_on_ice, 0)}</li>
-      <li>Goals: {playerDetails.goals}</li>
-      <li>Goals Per Game: {roundNum(playerDetails.goals_per_game, 3)}</li>
-      <li>Blocked Shots Per Game: {roundNum(playerDetails.blocked_shots_per_game, 3)}</li>
-      <li>Blocked Shots: {playerDetails.blocked}</li>
-      <li>Plus/Minus: {playerDetails.plusMinus}</li>
-      <li>Assists: {playerDetails.assists}</li>
-      <li>PIM: {playerDetails.penaltymin}</li>
-      <li>PIM Per Game: {roundNum(playerDetails.pim_per_game, 3)}</li>
-      <li>Points: {playerDetails.points}</li>
-      <li>Points Per Game: {roundNum(playerDetails.points_per_game, 3)}</li>
-      <li>Salary Cap Hit: &#36;{(playerDetails.cap_hit).toLocaleString()}</li>
-      <li>Cap Hit Percentage: {roundNum((playerDetails.cap_pct * 100), 1)}%</li>
-      {playerDetails.Defense_Skill ? <li>Defense Skill Rating: {playerDetails.Defense_Skill}</li> : ''}
-      {playerDetails.def_pass_rtg ? <li>Defensive Pass Rating: {playerDetails.fwd_pass_rtg}</li> : ''}
-      {playerDetails.def_scoring_rtg ? <li>Defensive Scoring Rating: {playerDetails.fwd_scoring_rtg}</li> : ''}
-      {playerDetails.def_shift_usage_rtg ? <li>Defensive Shift Usage Rating: {playerDetails.fwd_shift_usage_rtg}</li> : ''}
-      {playerDetails.penalty_liability_rtg ? <li>Penalty Liability Rating: {playerDetails.penalty_liability_rtg}</li> : ''}
-      {playerDetails.def_block_rtg ? <li>Defensive Block Rating: {playerDetails.def_block_rtg}</li> : ''}
+    <div className="past-details-card">
+      <li><b>Games:</b> {playerDetails.games}</li>
+      <li><b>Games Started:</b> {playerDetails.games_started}</li>
+      <li><b>Goals:</b> {playerDetails.goals}</li>
+      <li><b>Goals Per Game:</b> {roundNum(playerDetails.goals_per_game, 3)}</li>
+      <li><b>Blocked Shots Per Game</b> {roundNum(playerDetails.blocked_shots_per_game, 3)}</li>
+      <li><b>Blocked Shots:</b> {playerDetails.blocked}</li>
+      <li><b>Plus/Minus:</b> {playerDetails.plusMinus}</li>
+      <li><b>Assists:</b> {playerDetails.assists}</li>
+      <li><b>PIM:</b> {playerDetails.penaltymin}</li>
+      <li><b>PIM Per Game:</b> {roundNum(playerDetails.pim_per_game, 3)}</li>
+      <li><b>Points:</b> {playerDetails.points}</li>
+      <li><b>Points Per Game:</b> {roundNum(playerDetails.points_per_game, 3)}</li>
+      <li><b>Salary Cap Hit:</b> &#36;{(playerDetails.cap_hit).toLocaleString()}</li>
+      <li><b>Cap Hit Percentage:</b> {roundNum((playerDetails.cap_pct * 100), 1)}%</li>
+      {playerDetails.Defense_Skill ? <li><b>Defense Skill Rating:</b> {playerDetails.Defense_Skill}</li> : ''}
+      {playerDetails.def_pass_rtg ? <li><b>Defensive Pass Rating:</b> {playerDetails.def_pass_rtg}/10</li> : ''}
+      {playerDetails.def_scoring_rtg ? <li><b>Defensive Scoring Rating:</b> {playerDetails.def_scoring_rtg}/10</li> : ''}
+      {playerDetails.def_shift_usage_rtg ? <li><b>Defensive Shift Usage Rating:</b> {playerDetails.def_shift_usage_rtg}/10</li> : ''}
+      {playerDetails.penalty_liability_rtg ? <li><b>Penalty Liability Rating:</b> {playerDetails.penalty_liability_rtg}/10</li> : ''}
+      {playerDetails.def_block_rtg ? <li><b>Defensive Block Rating:</b> {playerDetails.def_block_rtg}/10</li> : ''}
     </div>
   );
 }
