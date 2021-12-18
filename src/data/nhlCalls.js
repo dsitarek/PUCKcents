3a6b0e1c081d9ab3b57a71ceb0193846e0bac83e
Daniel Sitarek <deltasierra777@gmail.com>
2021-12-17 20:31:15 -0600

    cleaning up code

--- src/data/nhlCalls.js
+++ src/data/nhlCalls.js
@@ -12,21 +12,6 @@ const removeDuplicatesFromArrayByProperty = (arr, prop) => arr.reduce((accumulat
   return accumulator;
 }, []);
 
-// const getSearchedPlayers = async (userSearch) => {
-//   const search = userSearch.replace(/\s/g, '%20');
-//   const searchCall = await axios.get(`https://suggest.svc.nhl.com/svc/suggest/v1/minplayers/${search}`);
-//   const searchedPlayerReturn = [];
-//   const playerDataArr = (searchCall.data.suggestions).map((player) => player.split('|'));
-//   playerDataArr.forEach((player) => searchedPlayerReturn.push({
-//     id: player[0],
-//     name: `${player[2]} ${player[1]}`,
-//     team: player[11],
-//     position: player[12],
-//     playerNumber: player[13],
-//   }));
-//   console.log(searchCall);
-//   return searchedPlayerReturn;
-// };
 const getCurrentInfo = async (playerId) => {
   const call = await axios.get(`${statsApi}/people/${playerId}`);
   return call.data.people[0];
@@ -48,7 +33,6 @@ const getSearchedPlayers = async (userSearch) => {
       position: player.position,
     });
   });
-  console.log(searchedPlayerReturn);
   return searchedPlayerReturn;
 };
 
