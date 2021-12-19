eeaf65bcfa85414a6cccd89a0171cdc9b7531bb8
Daniel Sitarek <deltasierra777@gmail.com>
2021-12-18 18:51:00 -0600

    changed sign in text

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -4,7 +4,7 @@ import logo from '../assets/logo3.png';
 export default function SignIn() {
   return (
     <div className="text-center mt-5">
-      <h1 className="sign-in-header">Welcome! You must be logged in to view your lines.</h1>
+      <h1 className="sign-in-header">Welcome! Please sign in to view or create lines.</h1>
       <img className="sign-in-img" src={logo} alt="logo" />
     </div>
   );
