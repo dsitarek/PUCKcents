17de235af7edebab614134382cd01b7f1bfd8d76
Daniel Sitarek <deltasierra777@gmail.com>
2021-12-20 16:27:07 -0600

    fixed bug for current stats on non-current players

--- src/components/CurrentDefenseCard.js
+++ src/components/CurrentDefenseCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import noImg from '../assets/noImg.png';
 
 export default function CurrentDefenseCard({ currentStats, currentInfo, playerImgURL }) {
   return (
     <div className="current-details-card">
-      {currentStats?.name
-        ? (
-          <div className="current-info-container">
-            <h2>{currentInfo.fullName}</h2>
-
-            <div className="details-current-header">
-              <div><img src={playerImgURL} className="player-img" alt={currentStats.name} /></div>
-              <div className="details-current-info">
+      <div className="current-info-container">
+        <h2>{currentInfo.fullName}</h2>
+        <div className="details-current-header">
+          <div><img src={playerImgURL} className="player-img" alt={currentInfo?.name || 'player'} onError={(e) => { e.target.onerror = null; e.target.src = noImg; }} /></div>
+          <div className="details-current-info">
+            {currentStats?.name
+              ? (
                 <ul>
                   <li><b>Current Team:</b> {currentInfo.currentTeam.name}</li>
                   <li><b>Position:</b> {currentInfo.primaryPosition.name}</li>
@@ -21,78 +21,79 @@ export default function CurrentDefenseCard({ currentStats, currentInfo, playerIm
                   <li><b>Weight:</b> {currentInfo.weight}lbs</li>
                   <li><b>Shoots:</b> {(currentInfo.shootsCatches).startsWith('R') ? 'Right' : 'Left'}</li>
                 </ul>
-              </div>
-            </div>
-
+              ) : <h4 className="no-stats-header">No Current Stats  For {currentInfo.fullName}</h4>}
           </div>
-        ) : <h4>No Current Stats  For {currentInfo.fullName}</h4>}
-      <h4 className="info-header">Current Season Stats</h4>
+        </div>
+      </div>
       {currentStats?.name
         ? (
-          <div className="details-current-body">
-            <div>
-              <table className="line-stats-table">
-                <thead>
-                  <tr>
-                    <th className="line-stats-table-header">Even Strength TOI</th>
-                    <th className="line-stats-table-header">Power Play Goals</th>
-                    <th className="line-stats-table-header">Power Play Points</th>
-                    <th className="line-stats-table-header">Power Play TOI</th>
-                    <th className="line-stats-table-header">Shorthanded Goals</th>
-                    <th className="line-stats-table-header">Shorthanded Points</th>
-                    <th className="line-stats-table-header">Shorthanded TOI</th>
-                    <th className="line-stats-table-header">PIM</th>
-                    <th className="line-stats-table-header">Total TOI</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="line-stats-table-data">{currentStats.evenTimeOnIcePerGame}</td>
-                    <td className="line-stats-table-data">{currentStats.powerPlayGoals}</td>
-                    <td className="line-stats-table-data">{currentStats.powerPlayPoints}</td>
-                    <td className="line-stats-table-data">{currentStats.powerPlayTimeOnIce}</td>
-                    <td className="line-stats-table-data">{currentStats.shortHandedGoals}</td>
-                    <td className="line-stats-table-data">{currentStats.shortHandedPoints}</td>
-                    <td className="line-stats-table-data">{currentStats.shortHandedTimeOnIce}</td>
-                    <td className="line-stats-table-data">{currentStats.pim}</td>
-                    <td className="line-stats-table-data">{currentStats.timeOnIce}</td>
-                  </tr>
-                </tbody>
-              </table>
+          <>
+            <h4 className="info-header">Current Season Stats</h4>
+            <div className="details-current-body">
+              <div>
+                <table className="line-stats-table">
+                  <thead>
+                    <tr>
+                      <th className="line-stats-table-header">Even Strength TOI</th>
+                      <th className="line-stats-table-header">Power Play Goals</th>
+                      <th className="line-stats-table-header">Power Play Points</th>
+                      <th className="line-stats-table-header">Power Play TOI</th>
+                      <th className="line-stats-table-header">Shorthanded Goals</th>
+                      <th className="line-stats-table-header">Shorthanded Points</th>
+                      <th className="line-stats-table-header">Shorthanded TOI</th>
+                      <th className="line-stats-table-header">PIM</th>
+                      <th className="line-stats-table-header">Total TOI</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="line-stats-table-data">{currentStats.evenTimeOnIcePerGame}</td>
+                      <td className="line-stats-table-data">{currentStats.powerPlayGoals}</td>
+                      <td className="line-stats-table-data">{currentStats.powerPlayPoints}</td>
+                      <td className="line-stats-table-data">{currentStats.powerPlayTimeOnIce}</td>
+                      <td className="line-stats-table-data">{currentStats.shortHandedGoals}</td>
+                      <td className="line-stats-table-data">{currentStats.shortHandedPoints}</td>
+                      <td className="line-stats-table-data">{currentStats.shortHandedTimeOnIce}</td>
+                      <td className="line-stats-table-data">{currentStats.pim}</td>
+                      <td className="line-stats-table-data">{currentStats.timeOnIce}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div>
+                <table className="line-stats-table">
+                  <thead>
+                    <tr>
+                      <th className="line-stats-table-header">Games</th>
+                      <th className="line-stats-table-header">Goals</th>
+                      <th className="line-stats-table-header">Assists</th>
+                      <th className="line-stats-table-header">Points</th>
+                      <th className="line-stats-table-header">Shots</th>
+                      <th className="line-stats-table-header">Shot %</th>
+                      <th className="line-stats-table-header">Plus/Minus</th>
+                      <th className="line-stats-table-header">Blocked Shots</th>
+                      <th className="line-stats-table-header">Hits</th>
+                      <th className="line-stats-table-header">Game Winning Goals</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="line-stats-table-data">{currentStats.games}</td>
+                      <td className="line-stats-table-data">{currentStats.goals}</td>
+                      <td className="line-stats-table-data">{currentStats.assists}</td>
+                      <td className="line-stats-table-data">{currentStats.points}</td>
+                      <td className="line-stats-table-data">{currentStats.shots}</td>
+                      <td className="line-stats-table-data">{currentStats.shotPct}</td>
+                      <td className="line-stats-table-data">{currentStats.plusMinus}</td>
+                      <td className="line-stats-table-data">{currentStats.blocked}</td>
+                      <td className="line-stats-table-data">{currentStats.hits}</td>
+                      <td className="line-stats-table-data">{currentStats.gameWinningGoals}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-            <div>
-              <table className="line-stats-table">
-                <thead>
-                  <tr>
-                    <th className="line-stats-table-header">Games</th>
-                    <th className="line-stats-table-header">Goals</th>
-                    <th className="line-stats-table-header">Assists</th>
-                    <th className="line-stats-table-header">Points</th>
-                    <th className="line-stats-table-header">Shots</th>
-                    <th className="line-stats-table-header">Shot %</th>
-                    <th className="line-stats-table-header">Plus/Minus</th>
-                    <th className="line-stats-table-header">Blocked Shots</th>
-                    <th className="line-stats-table-header">Hits</th>
-                    <th className="line-stats-table-header">Game Winning Goals</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="line-stats-table-data">{currentStats.games}</td>
-                    <td className="line-stats-table-data">{currentStats.goals}</td>
-                    <td className="line-stats-table-data">{currentStats.assists}</td>
-                    <td className="line-stats-table-data">{currentStats.points}</td>
-                    <td className="line-stats-table-data">{currentStats.shots}</td>
-                    <td className="line-stats-table-data">{currentStats.shotPct}</td>
-                    <td className="line-stats-table-data">{currentStats.plusMinus}</td>
-                    <td className="line-stats-table-data">{currentStats.blocked}</td>
-                    <td className="line-stats-table-data">{currentStats.hits}</td>
-                    <td className="line-stats-table-data">{currentStats.gameWinningGoals}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+          </>
         ) : ''}
     </div>
   );
--- src/components/CurrentGoalieCard.js
+++ src/components/CurrentGoalieCard.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import noImg from '../assets/noImg.png';
 
 export default function CurrentGoalieCard({
   currentStats, currentInfo, roundNum, playerImgURL,
 }) {
   return (
     <div className="current-details-card">
-      {currentStats?.name
-        ? (
-          <div className="current-info-container">
-            <h2>{currentInfo.fullName}</h2>
-            <div className="details-current-header">
-              <div><img src={playerImgURL} className="player-img" alt={currentStats.name} /></div>
-              <div className="details-current-info">
+      <div className="current-info-container">
+        <h2>{currentInfo.fullName}</h2>
+        <div className="details-current-header">
+          <div><img src={playerImgURL} className="player-img" alt={currentInfo?.name || 'player'} onError={(e) => { e.target.onerror = null; e.target.src = noImg; }} /></div>
+          <div className="details-current-info">
+            {currentStats?.name
+              ? (
                 <ul>
                   <li><b>Current Team:</b> {currentInfo.currentTeam.name}</li>
                   <li><b>Position:</b> {currentInfo.primaryPosition.name}</li>
@@ -22,71 +23,73 @@ export default function CurrentGoalieCard({
                   <li><b>Weight:</b> {currentInfo.weight}lbs</li>
                   <li><b>Catches:</b> {(currentInfo.shootsCatches).startsWith('R') ? 'Right' : 'Left'}</li>
                 </ul>
-              </div>
-            </div>
+              ) : <h4 className="no-stats-header">No Current Stats  For {currentInfo.fullName}</h4>}
           </div>
-        ) : ''}
-      <h4 className="info-header">Current Season Stats</h4>
+        </div>
+      </div>
       {currentStats?.name
         ? (
-          <div className="details-current-body">
-            <div>
-              <table className="line-stats-table">
-                <thead>
-                  <tr>
-                    <th className="line-stats-table-header">Games</th>
-                    <th className="line-stats-table-header">Wins</th>
-                    <th className="line-stats-table-header">Losses</th>
-                    <th className="line-stats-table-header">Shutouts</th>
-                    <th className="line-stats-table-header">Save %</th>
-                    <th className="line-stats-table-header">Saves</th>
-                    <th className="line-stats-table-header">GAA</th>
-                    <th className="line-stats-table-header">Goals Against</th>
-                    <th className="line-stats-table-header">Games Started</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="line-stats-table-data">{currentStats.games}</td>
-                    <td className="line-stats-table-data">{currentStats.wins}</td>
-                    <td className="line-stats-table-data">{currentStats.losses}</td>
-                    <td className="line-stats-table-data">{currentStats.shutouts}</td>
-                    <td className="line-stats-table-data">{roundNum(currentStats.savePercentage, 3)}</td>
-                    <td className="line-stats-table-data">{currentStats.saves}</td>
-                    <td className="line-stats-table-data">{roundNum(currentStats.goalAgainstAverage, 3)}</td>
-                    <td className="line-stats-table-data">{currentStats.goalsAgainst}</td>
-                    <td className="line-stats-table-data">{currentStats.timeOnIce}</td>
-                  </tr>
-                </tbody>
-              </table>
+          <>
+            <h4 className="info-header">Current Season Stats</h4>
+            <div className="details-current-body">
+              <div>
+                <table className="line-stats-table">
+                  <thead>
+                    <tr>
+                      <th className="line-stats-table-header">Games</th>
+                      <th className="line-stats-table-header">Wins</th>
+                      <th className="line-stats-table-header">Losses</th>
+                      <th className="line-stats-table-header">Shutouts</th>
+                      <th className="line-stats-table-header">Save %</th>
+                      <th className="line-stats-table-header">Saves</th>
+                      <th className="line-stats-table-header">GAA</th>
+                      <th className="line-stats-table-header">Goals Against</th>
+                      <th className="line-stats-table-header">Games Started</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="line-stats-table-data">{currentStats.games}</td>
+                      <td className="line-stats-table-data">{currentStats.wins}</td>
+                      <td className="line-stats-table-data">{currentStats.losses}</td>
+                      <td className="line-stats-table-data">{currentStats.shutouts}</td>
+                      <td className="line-stats-table-data">{roundNum(currentStats.savePercentage, 3)}</td>
+                      <td className="line-stats-table-data">{currentStats.saves}</td>
+                      <td className="line-stats-table-data">{roundNum(currentStats.goalAgainstAverage, 3)}</td>
+                      <td className="line-stats-table-data">{currentStats.goalsAgainst}</td>
+                      <td className="line-stats-table-data">{currentStats.timeOnIce}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div>
+                <table className="line-stats-table">
+                  <thead>
+                    <tr>
+                      <th className="line-stats-table-header">Even Strength Save %</th>
+                      <th className="line-stats-table-header">Even Strength Saves</th>
+                      <th className="line-stats-table-header">Power Play Save %</th>
+                      <th className="line-stats-table-header">Power Play Saves</th>
+                      <th className="line-stats-table-header">Shorthanded Save %</th>
+                      <th className="line-stats-table-header">Shorthanded Saves</th>
+                      <th className="line-stats-table-header">Games Started</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td className="line-stats-table-data">{roundNum(currentStats.evenStrengthSavePercentage / 100, 3)}</td>
+                      <td className="line-stats-table-data">{currentStats.evenSaves}</td>
+                      <td className="line-stats-table-data">{roundNum(currentStats.powerPlaySavePercentage / 100, 3)}</td>
+                      <td className="line-stats-table-data">{currentStats.powerPlaySaves}</td>
+                      <td className="line-stats-table-data">{roundNum(currentStats.shortHandedSavePercentage / 100, 3)}</td>
+                      <td className="line-stats-table-data">{currentStats.shortHandedSaves}</td>
+                      <td className="line-stats-table-data">{currentStats.gamesStarted}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
             </div>
-            <div>
-              <table className="line-stats-table">
-                <thead>
-                  <tr>
-                    <th className="line-stats-table-header">Even Strength Save %</th>
-                    <th className="line-stats-table-header">Even Strength Saves</th>
-                    <th className="line-stats-table-header">Power Play Save %</th>
-                    <th className="line-stats-table-header">Power Play Saves</th>
-                    <th className="line-stats-table-header">Shorthanded Save %</th>
-                    <th className="line-stats-table-header">Shorthanded Saves</th>
-                    <th className="line-stats-table-header">Games Started</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td className="line-stats-table-data">{roundNum(currentStats.evenStrengthSavePercentage / 100, 3)}</td>
-                    <td className="line-stats-table-data">{currentStats.evenSaves}</td>
-                    <td className="line-stats-table-data">{roundNum(currentStats.powerPlaySavePercentage / 100, 3)}</td>
-                    <td className="line-stats-table-data">{currentStats.powerPlaySaves}</td>
-                    <td className="line-stats-table-data">{roundNum(currentStats.shortHandedSavePercentage / 100, 3)}</td>
-                    <td className="line-stats-table-data">{currentStats.shortHandedSaves}</td>
-                    <td className="line-stats-table-data">{currentStats.gamesStarted}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+          </>
         ) : ''}
     </div>
   );
